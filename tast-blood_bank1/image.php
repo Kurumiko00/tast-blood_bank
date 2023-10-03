ce646e483079c9692b6271be2a2c108a168b1c0c
Kurumiko00 <109063491+Kurumiko00@users.noreply.github.com>
2023-10-03 20:04:43 +0700

    Add files via upload

---
+++ tast-blood_bank1/image.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+    <a href="hospital-1.html" >
+        <img src="img/1.jpg" width="300px">
+    </a>
+        <hr>
+        <a href="hospital-2.html" >
+        <img src="img/2.jpg" width="300px">
+    </a>
+        <hr>
+        <a href="hospital-3.html" >
+        <img src="img/3.jpg" width="300px">
+    </a>
+      
+        <script src="" async defer></script>
+    </body>
+</html>
---
+++ tast-blood_bank1/login.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+require('connect.php');
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // ค้นหาข้อมูลผู้ใช้งานในฐานข้อมูล
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // การลงชื่อเข้าใช้งานสำเร็จ
+        echo "ลงชื่อเข้าใช้งานสำเร็จ";
+        header("Location: image.php");
+        exit;
+
+        
+    } else {
+        // การลงชื่อเข้าใช้งานล้มเหลว
+        echo "ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง";
+    }
+}
+
+// ปิดการเชื่อมต่อฐานข้อมูล
+$conn->close();
+?>
+
