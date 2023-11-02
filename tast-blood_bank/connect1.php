e77b27ec77b49956bfbbc03eb31cef110d1d0373
Kurumiko00 <109063491+Kurumiko00@users.noreply.github.com>
2023-11-02 22:47:16 +0700

    Delete tast-blood_bank directory

--- tast-blood_bank/connect1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "Kurumiko";
-$password = "Chinnawat02";
-$database = "blood_bank1";
-
-$conn = new mysqli($servername, $username, $password, $database);
-
-?>
